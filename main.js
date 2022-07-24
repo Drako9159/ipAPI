22c0acae1eb4531ae2728f305719e642bc115b8a
drako9159 <chochito9159@gmail.com>
2022-07-24 01:22:27 -0500

    new tool

---
+++ main.js
@@ -0,0 +1,29 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '9723ce58bfmshc59493759ceedd7p1d2f73jsnd22320cea70b',
+		'X-RapidAPI-Host': 'ip-geolocation-and-threat-detection.p.rapidapi.com'
+	}
+};
+const fetchIpInfo = ip =>{
+    return fetch(`https://ip-geolocation-and-threat-detection.p.rapidapi.com/${ip}`, options).then(res => res.json())
+    .catch(err => console.error(err))
+}
+const $ = selector => document.querySelector(selector);
+const $form = $("#form");
+const $input = $("#input");
+const $submit = $("#submit");
+const $results = $("#results");
+$form.addEventListener('submit', async (event) => {
+    event.preventDefault()//Evita el refres con POST
+    const {value} = $input
+    if(!value) return
+    $submit.setAttribute("disabled", '')
+    $submit.setAttribute("aria-busy", "true")
+    const ipInfo = await fetchIpInfo(value);
+    if(ipInfo){
+        $results.innerHTML = JSON.stringify(ipInfo, null, 2);
+    }
+    $submit.removeAttribute("disabled")
+    $submit.removeAttribute("aria-busy", "true")
+})
